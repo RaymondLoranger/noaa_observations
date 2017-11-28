253c7468e53747c078ccb2ecfb5b30f4ac85d6e4
RaymondLoranger <Raymond.Loranger@bell.net>
2017-11-28 00:39:18 -0500

    doc touch-ups

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NOAA.Observations.Mixfile do
   def project() do
     [
       app: :noaa_observations,
-      version: "0.3.0",
+      version: "0.3.1",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
       name: "NOAA Observations",
