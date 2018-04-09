8a20e2c3a3e69864cfa082924c3132478b4cf461
RaymondLoranger <Raymond.Loranger@bell.net>
2018-04-09 10:28:03 -0400

    0.4.11

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NOAA.Observations.Mixfile do
   def project do
     [
       app: :noaa_observations,
-      version: "0.4.10",
+      version: "0.4.11",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       name: "NOAA Observations",
@@ -49,8 +49,8 @@ defmodule NOAA.Observations.Mixfile do
     [
       {:mix_tasks,
        github: "RaymondLoranger/mix_tasks", only: :dev, runtime: false},
-      {:persist_config, "~> 0.1"},
-      {:io_ansi_table, "~> 0.3"},
+      {:persist_config, "~> 0.2"},
+      {:io_ansi_table, "~> 0.4"},
       {:earmark, "~> 1.0", only: :dev},
       {:ex_doc, "~> 0.14", only: :dev, runtime: false},
       {:dialyxir, "~> 0.5", only: :dev, runtime: false},
